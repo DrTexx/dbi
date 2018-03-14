f2e4156dbd06a4716ab0a586833cd86df7490428
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2018-03-14 13:28:55 +1100

    Need to unbreak in-function code processing
    I had previously made text append to a line after a command to the left of it in arguments was run, however I seem to have broken this at some point. Any ideas?

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from src import dbi
+import time
 
 # dictionary containing debugging settings
-db = {'debug_active': True, 'verbosity_level': 3}
+db = {'debug_active': True, 'verbosity_level': 1}
 # run dbi with the set parameters
-dbi.dbi(db,3,"10+1 is",print(pow(10,2) + pow(50,7)),"okay?")
+dbi.dbi(db,3,"verb of 3")
 dbi.dbi(db,2,"verb of 2")
 dbi.dbi(db,1,"verb of 1")
+dbi.dbi(db,1,"waiting for 3 seconds...",time.sleep(3),"finished waiting!")
