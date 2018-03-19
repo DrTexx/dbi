17b0b6f276ffc2131e897f2b1c58664f65dbee09
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2018-03-19 16:25:41 +1100

    semi-fixed

--- dbi.py
+++ dbi.py
@@ -19,27 +19,27 @@ now = datetime.datetime.now
 # define functions
 def dbi(db,min_level,*args):
     if db['debug_active'] and min_level <= db['verbosity_level']: # check debug is True and level is okay
-        prefixes = {'1': "lvl1!",
-                    '2': "lvl2!",
-                    '3': "lvl3!"}
+        prefixes = {'1': ct("lvl1!",Fore="GREEN"),
+                    '2': ct("lvl2!",Fore="YELLOW"),
+                    '3': ct("lvl3!",Fore="RED")}
         al = [item for item in args]
         sys.stdout.write("[%s][%s]: " % (now().isoformat('T'),prefixes[str(db['verbosity_level'])])) # write standard time stamp
         for i in range(len(al)): # for each argument supplied
-            print("i =",i)
+            #print("i =",i)
             if isinstance(al[i],str): # if this arg is a string
-                print(al[i],"is a string")
+                #print(al[i],"is a string")
                 sys.stdout.write(al[i])
                 if (len(al) - 1 < 0): # if a previous item exists
                     print("there's an item before this one")
                     if (isinstance(al[i + 1],str)): # if next arg is a string
-                        print("there's another string after this one")
+                        #print("there's another string after this one")
                         sys.stdout.write(" | ")
                     else: # if the next arg isn't a string
                         pass
                 else: # this is the last item in the list
                     pass
             else: # if this arg isn't a string
-                print("is not a string:",al[i])
+                #print("is not a string:",al[i])
                 try: al[i] # try executing it
                 except: raise Exception("COULDN'T RUN CODE IN DBI!") # otherwise raise an exception
         print("")
@@ -55,7 +55,9 @@ if(runThisTest):
             time.sleep(1)
             my_dict['bool'] = False
     
-    db = {'debug_active': True, 'verbosity_level': 1}
+    db = {'debug_active': True, 'verbosity_level': 3}
     print(test['bool'])
-    dbi(db,1,"hello!","changing...",change(test),"changed!","goodbye!")
+    dbi(db,1,"testing level 1 verb...",change(test),"changed!")
+    dbi(db,2,"testing level 2 verb...",change(test),"changed!")
+    dbi(db,3,"testing level 3 verb...",change(test),"changed!")
     print(test['bool'])
