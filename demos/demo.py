6ff808786b904f5c8152846ce205f8936fc6b6ac
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2018-03-31 16:56:12 +1100

    re-added demos

---
+++ demos/demo.py
@@ -0,0 +1,17 @@
+'''
+Created on 19 Mar. 2018
+
+@author: Denver
+'''
+
+from dbi.dbi import dbi
+db = {'debug_active': True,'verbosity_level': 3,'db_imports': ['demo_tools']}
+
+dbi(db,1,"now we'll wait","just for 1 second","!EXEC!:demo_tools.sf1()","finished command!")
+dbi(db,1,"!EXEC!:demo_tools.RNG()","generated a random number!: ","!EXEC!:demo_tools.printRNG()")
+dbi(db,2,"now we'll wait","!EXEC!:demo_tools.sf3()","finished command!")
+dbi(db,3,"now we'll wait","!EXEC!:demo_tools.sf1()","finished command!")
+dbi(db,3,"now we'll wait","!EXEC!:demo_tools.sf1()","finished command!")
+dbi(db,2,"now we'll wait","!EXEC!:demo_tools.sf1()","finished command!")
+dbi(db,2,"now we'll wait","!EXEC!:demo_tools.sf1()","finished command!")
+dbi(db,1,"now we'll wait","!EXEC!:demo_tools.sf1()","finished command!")
