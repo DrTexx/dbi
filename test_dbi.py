8d06b4de264c394e6bc06c10cdb6dbff1d3e8381
Denver P <23412100+DrTexxOfficial@users.noreply.github.com>
2018-12-09 19:14:40 +1100

    test failed build + extra coverage

--- test_dbi.py
+++ test_dbi.py
@@ -4,10 +4,19 @@ dbi = Dbi(3,True)
 dpm = dbi.print_message
 
 class TestDbi():
-    def test_verblvl(self):
+    def test_verb_type(self):
         with pytest.raises(TypeError):
             dpm("yes","sir")
         with pytest.raises(TypeError):
             dpm(3.4,"yes","sir")
+    def test_verb_limits(self):
+        with pytest.raises(ValueError):
+            dpm(-1,"hello","chap")
+        with pytest.raises(ValueError):
+            dpm(0,"mr.","zero")
+    def test_arg_type(self):
         with pytest.raises(TypeError):
-            dpm(3,"actually","this should work")
+            dpm(3,"I'm",45)
+        with pytest.raises(TypeError):
+            dpm(3,2,"turtle doves and a partridge in a pair tree")
+            
